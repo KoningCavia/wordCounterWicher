65cf78d1bad991c44d87d31932b8594cf9b9740e
wicher <wicherjvos@gmail.com>
2022-08-08 14:16:17 +0200

    added functional WordFrequencyAnalyzerClass.calculateHighestFrequency

--- src/main/java/models/WordFrequencyAnalyzerClass.java
+++ src/main/java/models/WordFrequencyAnalyzerClass.java
@@ -8,7 +8,28 @@ import java.util.List;
 public class WordFrequencyAnalyzerClass implements WordFrequencyAnalyzer {
     @Override
     public int calculateHighestFrequency(String text) {
-        return 0;
+        String[] words = text.split("[^a-zA-Z']");                  // TODO should this be arraylist?
+
+        int highestFreq = 0;
+        int currentFreq;
+
+        for (String word1: words) {
+            String word1Lower = word1.toLowerCase();
+            currentFreq = 0;
+
+            for (String word2: words) {
+                String word2Lower = word2.toLowerCase();
+
+                if (word1Lower.equals(word2Lower)) {
+                    currentFreq++;
+                };
+                if (currentFreq > highestFreq) {
+                    highestFreq = currentFreq;
+                };
+            }
+        }
+
+        return highestFreq;
     }
 
     @Override
